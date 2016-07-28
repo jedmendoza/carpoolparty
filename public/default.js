537ddfc313251cde53fbc8c7fcef46b09e811217
Jed Mendoza <jed.mendoza@gmail.com>
2016-07-28 16:27:52 -0700

    Working on creating carpool feature.

--- public/default.js
+++ public/default.js
@@ -1,6 +1,30 @@
 var makeCarpoolBtn = document.getElementById('carpool-btn');
 var carpoolDiv = document.getElementById('carpool-info');
+var cancel = document.getElementById('cancel-ride');
+var rideInfo = document.getElementById('ride-info');
+var submitRide = document.getElementById('submit-ride');
+var festival = document.getElementById('festival-name');
+// var rideInfo = document.getElementById('ride-info');
+
 
 makeCarpoolBtn.addEventListener('click', function(e) {
   $('#carpool-info').removeClass('hidden')
-})
+});
+
+submitRide.addEventListener('click', function(e) {
+  var rideDetails = [];
+  rideDetails.venue = festival.value;
+  rideDetails.info = rideInfo.value;
+  console.log(rideDetails);
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', '/carpool');
+  xhr.setRequestHeader('')
+  xhr.send()
+});
+
+cancel.addEventListener('click', function(e) {
+  rideInfo.value = '';
+  festiv
+  $('#carpool-info').addClass('hidden')
+});
