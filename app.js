ae7b295bb24be3a2b5e5bad63172bde7ec7544c7
Jed Mendoza <jed.mendoza@gmail.com>
2016-07-28 15:28:40 -0700

    Added POST route

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var bodyParser = require('body-parser').json();
 var Client = require('mongodb').MongoClient;
 var app = express();
 
+var url = 'mongodb://localhost:27017/carpoolparty';
+
+
 app.use(express.static('./public'));
 
 app.get('/', function(req, res) {
@@ -10,8 +13,15 @@ app.get('/', function(req, res) {
 });
 
 app.post('/carpool', function(req, res) {
-  console.log('post route is working')
-  res.send('')
+  Client.connect(url, function(error, db) {
+    if (error) {
+      console.log(error);
+      res.send(404)
+    } else {
+      res.send()
+      console.log('post route is working')
+    }
+  })
 });
 
 app.listen(8080);
